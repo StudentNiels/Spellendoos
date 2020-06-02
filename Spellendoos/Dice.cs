303a745e7861071b4389c81218e9b3791f0515c6
Altheric <mauricedx@gmail.com>
2020-06-02 09:41:28 +0200

    Added Dice & Dicetray and created basic functionality

---
+++ Spellendoos/Dice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spellendoos
+{
+    class Dice
+    {
+        private int eyes;
+        private Random rand;
+        public Dice(int eyes) 
+        {
+            //Determines the amount of eyes the dice has, while for the games we are doing the amount should be always 6. Its nice to  be able to determine these things.
+            this.eyes = eyes;
+            //instance the Random class to randomly do random things like rolling a dice.
+            rand = new Random();
+        }
+
+        public int RollDice() 
+        {
+            //Add a one to the amount of eyes due to the way Next() works with indexes
+            return rand.Next(0, eyes + 1);
+        }
+    }
+}
---
+++ Spellendoos/DiceTray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spellendoos
+{
+    class DiceTray
+    {
+        //List of Dice objects
+        public List<Dice> Dices;
+
+        public DiceTray() 
+        {
+            //Create a list of dices to roll them all at once.
+            Dices = new List<Dice>();
+        }
+        public int[] RollDices()
+        {
+            //Create an empty array for the dice results
+            int[] diceResults = new int[Dices.Count];
+            //Index so we can add more dices to the int[]
+            int currentIndex = 0;
+            //Note that with this code, the index of Dices is the same as the index as diceResults.
+            foreach (Dice Dice in Dices) 
+            {
+                diceResults[currentIndex] = Dice.RollDice();
+            }
+            return diceResults;
+        } 
+    }
+}
