bd8335df8a6d9fcc5cec6ef2d57564dea6269ffe
Altheric <mauricedx@gmail.com>
2020-06-05 09:40:54 +0200

    Fixed RNG... hopefully

---
+++ Spellendoos/Dice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spellendoos
+{
+    class Dice
+    {
+        //Determines the amount of eyes the dice has, while for the games we are doing the amount should be always 6. Its nice to  be able to determine these things.
+        private int numberOfEyes;
+        //instance of the Random class to randomly do random things like rolling a dice.
+        private Random rand;
+        public Dice(int numberOfEyes) 
+        {
+            this.numberOfEyes = numberOfEyes;  
+            rand = new Random();
+        }
+
+        public int RollDice() 
+        {
+            //Add a one to the amount of eyes due to the way Next() works with indexes
+            return rand.Next(0, numberOfEyes + 1);
+        }
+    }
+}
---
+++ Spellendoos/Yahtzee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spellendoos
+{
+    internal class Yahtzee : ChanceGame
+    {
+        public override void AddPlayer(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetGameName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemovePlayer(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void StartGame(string name, List<Player> PlayerList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DiceTray ThrowDice(DiceTray dices)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
