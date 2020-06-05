0806f7436f2e98d118f477182526971beebaa883
StudentNiels <43601448+StudentNiels@users.noreply.github.com>
2020-06-05 09:41:20 +0200

    classes padwissel

--- Spellendoos/Classes/Player.cs
+++ Spellendoos/Classes/Player.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
-namespace Spellendoos.Classes
+namespace Spellendoos
 {
-    class Player
+    public class Player
     {
+        public string playerName;
+        public int points;
+        public string colour;
+
+
+        public Player(String name)
+        {
+            this.playerName = name;
+            this.points = 0;
+            this.colour = "Empty";
+        }
+
+        public string getPlayerName()
+        {
+            return this.playerName;
+        }
+
+        public void setPlayerName(string playerName)
+        {
+            this.playerName = playerName;
+        }
+
+        public int getPoints()
+        {
+            return this.points;
+        }
+
+        public void setPoints(int points)
+        {
+            this.points = points;
+        }
+
+        public string getColour()
+        {
+            return this.colour;
+        }
+
+        public void setColour(string colour)
+        {
+            this.colour = colour;
+        }
     }
 }
