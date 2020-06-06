487a3f4dc7c3017bd8ddc3193831f7734b252a9c
Josekappert1993 <jose_kappert1993@hotmail.com>
2020-06-06 14:04:32 +0200

    dit werkt

--- Spellendoos/ChanceGame.cs
+++ Spellendoos/ChanceGame.cs
@@ -11,22 +11,19 @@ namespace Spellendoos
         private string name;
         private List<Player> player;
         private DiceTray dices;
-        private Score score;
 
         public string Name { get => name; set => name = value; }
         public List<Player> Player { get => player; set => player = value; }
         internal DiceTray Dices { get => dices; set => dices = value; }
-        public Score Score { get => score; set => score = value; }
 
-        protected ChanceGame(string name, List<Player> player, DiceTray dices, Score score)
+        protected ChanceGame(string name, List<Player> player, DiceTray dices)
         {
             this.name = name;
             this.player = player;
             this.dices = dices;
-            this.score = score;
         }
 
-        public abstract void StartGame(String name, List<Player>);
+        public abstract void StartGame(String name, List<Player> players);
         public abstract void RemovePlayer(Player player);
         public abstract void AddPlayer(Player player);
         public abstract DiceTray ThrowDice(DiceTray dices);
