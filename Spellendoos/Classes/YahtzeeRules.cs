d56446fcab9f4bd63e889a0511cd7f13ed4147e1
Josekappert1993 <jose_kappert1993@hotmail.com>
2020-06-11 14:18:00 +0200

    Final update YahtzeeRules

--- Spellendoos/Classes/YahtzeeRules.cs
+++ Spellendoos/Classes/YahtzeeRules.cs
@@ -11,6 +11,7 @@ namespace Spellendoos.Classes
     class YahtzeeRules
     {
         private List<string> rules;
+        private List<string> rule_options;
         private List<int> score_options;
         public YahtzeeRules()
         {
@@ -32,7 +33,7 @@ namespace Spellendoos.Classes
         {
             int one = 0, two = 0, three = 0, four = 0, five = 0, six = 0;
 
-            List<string> ruleOptions = new List<string>();
+            this.rule_options = new List<string>();
 
             foreach(Dice dice in thrownDices)
             {
@@ -61,24 +62,24 @@ namespace Spellendoos.Classes
             //checked if number appears more than three times
             if(one >= 3 || two >= 3 || three >= 3 || four >= 3 || five >= 3 || six >= 3)
             {
-                ruleOptions.Add("Drie gelijk");
+                this.rule_options.Add("Drie gelijk");
                 score_options.Add(1 * one + 2 * two + 3 * three + 4 * four + 5 * five + 6 * six);
                 //checked if number appears more than three times and the others have the same value
                 if (one >= 2 || two >= 2 || three >= 2 || four >= 2 || five >= 2 || six >= 2)
                 {
-                    ruleOptions.Add("Full house");
+                    this.rule_options.Add("Full house");
                     score_options.Add(25);
                 }
                 //checked if number appears more than four times
                 if(one >= 4 || two >= 4 || three >= 4 || four >= 4 || five >= 4 || six >= 4)
                 {
-                    ruleOptions.Add("Vier gelijk");
+                    this.rule_options.Add("Vier gelijk");
                     score_options.Add(1 * one + 2 * two + 3 * three + 4 * four + 5 * five + 6 * six);
                 }
                 //checked if number appears five times
                 if (one == 5 || two == 5 || three == 5 || four == 5 || five == 5 || six == 5)
                 {
-                    ruleOptions.Add("Yahtzee");
+                    this.rule_options.Add("Yahtzee");
                     score_options.Add(50);
                 }
             }
@@ -90,11 +91,11 @@ namespace Spellendoos.Classes
                     {
                         if(four == 1)
                         {
-                            ruleOptions.Add("Kleine straat");
+                            this.rule_options.Add("Kleine straat");
                             score_options.Add(30);
                             if(five == 1) 
                             {
-                                ruleOptions.Add("Grote straat");
+                                this.rule_options.Add("Grote straat");
                                 score_options.Add(40);
                             }
                         }
@@ -109,11 +110,11 @@ namespace Spellendoos.Classes
                     {
                         if (five == 1)
                         {
-                            ruleOptions.Add("Kleine straat");
+                            this.rule_options.Add("Kleine straat");
                             score_options.Add(30);
                             if (six == 1)
                             {
-                                ruleOptions.Add("Grote straat");
+                                this.rule_options.Add("Grote straat");
                                 score_options.Add(40);
                             }
                         }
@@ -128,13 +129,21 @@ namespace Spellendoos.Classes
                     {
                         if (six == 1)
                         {
-                            ruleOptions.Add("Kleine straat");
+                            this.rule_options.Add("Kleine straat");
                             score_options.Add(30);
                         }
                     }
                 }
             }
-            
+            getScoreOption();
+            getRuleOption();
+        }
+
+        public List<int> getScoreOption() => this.score_options;
+
+        public List<string> getRuleOption()
+        {
+            return this.rule_options;
         }
     }
 
