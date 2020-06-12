79b9c75c8d488ef1a687eb7186961cbecabd1114
Josekappert1993 <jose_kappert1993@hotmail.com>
2020-06-12 09:10:25 +0200

    Mens erger je niet start

---
+++ Spellendoos/Classes/BoardGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spellendoos.Classes
+{
+    abstract class BoardGame
+    {
+        //Name of the game.
+        public string name;
+        //List of players.
+        public List<Player> players;
+        //Dices in the game.
+        public Dice dices;
+        //Array of score in the game
+        public int[] score;
+    }
+}
---
+++ Spellendoos/MensErgerJeNiet.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Spellendoos
+{
+    /// <summary>
+    /// Interaction logic for MensErgerJeNiet.xaml
+    /// </summary>
+    public partial class MensErgerJeNiet : Window
+    {
+        public MensErgerJeNiet()
+        {
+            InitializeComponent();
+        }
+
+        private void ThrowDice(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
