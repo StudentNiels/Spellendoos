07e252958c72e7a73ce564e0ed0cdbf23ad01204
Mart Ahlers <mart-ahlers@home.nl>
2020-06-19 15:08:54 +0200

    added Score

---
+++ Spellendoos/Classes/Score.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Spellendoos.Classes
+{
+    class Score : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private List<Player> players;
+        private Player selectedPlayer;
+
+        public Score()
+        {
+            players = MainWindow.getPlayers();
+        }
+
+        public List<Player> PlayerList
+        {
+            get { return players; }
+            set
+            {
+                players = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public Player SelectedPlayer
+        {
+            get { return selectedPlayer; }
+            set
+            {
+                selectedPlayer = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
