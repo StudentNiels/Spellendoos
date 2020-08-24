44d2ee902de7baab3a07d771b18d5d3f7c9a651d
Josekappert1993 <jose_kappert1993@hotmail.com>
2020-08-24 12:05:10 +0200

    Window mens erger je niet werkt

--- Spellendoos/MensErgerJeNietUI.xaml.cs
+++ Spellendoos/MensErgerJeNietUI.xaml.cs
@@ -12,19 +12,46 @@ namespace Spellendoos
     public partial class MensErgerJeNietUI : Window
     {
         private MensErgerJeNietWithUI MEJN;
-        private string playerName;
 
-        public MensErgerJeNietUI(List<Player> players)
+        public MensErgerJeNietUI(string gameName, List<Player> players)
         {
-            //this
-            playerName = "test";
-
             InitializeComponent();
+            //Player's names are set on the UI
+            player1Field.Text = players[0].getPlayerName();
+            string str1 = player1Field.Text;
+            char[] spearator1 = { ' ' };
+            String[] splitStr1 = str1.Split(spearator1);
+            player1Field.Text = splitStr1[1];
+            players[0].setPlayerName(player1Field.Text);
+
+            player2Field.Text = players[1].playerName;
+            string str2 = player2Field.Text;
+            char[] spearator2 = { ' ' };
+            String[] splitStr2 = str2.Split(spearator2);
+            player2Field.Text = splitStr2[1];
+            players[1].setPlayerName(player2Field.Text);
+
+            player3Field.Text = players[2].playerName;
+            string str3 = player3Field.Text;
+            char[] spearator3 = { ' ' };
+            String[] splitStr3 = str3.Split(spearator3);
+            player3Field.Text = splitStr3[1];
+            players[2].setPlayerName(player3Field.Text);
+
+            player4Field.Text = players[3].playerName;
+            string str4 = player4Field.Text;
+            char[] spearator4 = { ' ' };
+            String[] splitStr4 = str4.Split(spearator4);
+            player4Field.Text = splitStr4[1];
+            players[3].setPlayerName(player4Field.Text);
+
+            
         }
 
-        private void InitializeComponent()
+        private void InitializeComponent(string gameName, List<Player> players)
         {
-            throw new NotImplementedException();
+            this.MEJN = new MensErgerJeNietWithUI("Mens erger je niet", players);
+            
         }
 
         private void Throw_dice_Click(object sender, RoutedEventArgs e)
@@ -34,7 +61,7 @@ namespace Spellendoos
 
         private void quit_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Hide();
         }
     }
 }
